8e40f7cf1cd3ecdbc124c74a9a5e4cdf1446d357
Richard Aydan Fiuza Ost <122907674+aydanfiuza@users.noreply.github.com>
2023-04-06 15:59:44 -0300

    função adicionada

--- apiYouTube.py
+++ apiYouTube.py
@@ -7,18 +7,32 @@ developer_key = apiKey
 
 youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=apiKey)
 
-request_views = youtube.videos().list(
+def breakLine():
+    print("\n")
+
+request_gema = youtube.videos().list(
     part = 'snippet,contentDetails,statistics',
     id = 'vA7DyyWpvgk'
 )
 
-request_name = youtube.videos().list(
-    part = 'snippet,contentDetails,statistics',
-    id = 'vA7DyyWpvgk'
+request_estrogenio = youtube.videos().list(
+    part ='snippet,contentDetails,statistics',
+    id = 'JUVXsS8k5qs'
 )
 
-response_name = request_name.execute()
+def gemaRequest():
+    response_views = request_gema.execute()
+    return response_views['items'][0]['statistics']['likeCount']
+
+response_estrogenio = request_estrogenio.execute()
+print("Título do vídeo:", response_estrogenio['items'][0]['snippet']['title'])
+response_estrogenio = request_estrogenio.execute()
+print("Número de views:", response_estrogenio['items'][0]['statistics']['viewCount'])
+response_estrogenio = request_estrogenio.execute()
+print("Número de likes:", response_estrogenio['items'][0]['statistics']['likeCount'])
+breakLine()
+response_name = request_gema.execute()
 print("Título do vídeo:", response_name['items'][0]['snippet']['title'])
-
-response_views = request_views.execute()
+response_views = request_gema.execute()
 print("Número de views:", response_views['items'][0]['statistics']['viewCount'])
+print(gemaRequest())
